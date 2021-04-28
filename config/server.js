4c681273d6b7ee70f1ab13643ff3c9b414b95544
Oleksii Demchenko <o.demchenko@wrenchtech.io>
2021-04-28 15:01:39 +0300

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'fb62fb81aa73eb74069dc3449724cb0e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '463aff85-6afd-4672-b5bd-b2685fee0c73'
+};
